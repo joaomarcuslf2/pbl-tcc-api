48ccab0f495c275594cd5168c4ec34c80752621f
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2019-11-01 19:27:03 -0300

    Added migration for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  after_initialize :default_values
   has_secure_password
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
@@ -7,4 +8,8 @@ class User < ApplicationRecord
             length: { minimum: 6 },
             if: -> { new_record? || !password.nil? }
 
+  private
+    def default_values
+      self.role ||= "user"
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_01_191214) do
+ActiveRecord::Schema.define(version: 2019_11_01_221810) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_11_01_191214) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role", default: "user", null: false
   end
 
 end
